d56ff5efb8f439e52384033c0e938eeacd17715a
Alexander Isaev <IsaevAV27@gmail.com>
2021-12-06 12:47:39 +0100

    Refactoring @ advent_06.py

--- 2021/day_06/advent_06.py
+++ 2021/day_06/advent_06.py
@@ -9,9 +9,9 @@ t = deque([0]*9)
 for timer in sys.stdin.read().split(','):
     t[int(timer)] += 1
 
-for i in range(0, n):
+for i in range(n):
+    t[7] += t[0]
     t.rotate(-1)
-    t[6] += t[-1]
 
 print(' '.join([str(timer) for timer in t]))
 print(sum(t))
