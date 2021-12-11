8d40b944283480067eb33b417074be21840681e0
Alexander Isaev <isaevav27@gmail.com>
2021-12-11 12:58:40 +0100

    2021 Fix the unfinished refactoring @ day 7

--- 2021/day_07/advent_07.py
+++ 2021/day_07/advent_07.py
@@ -11,21 +11,35 @@ idx = defaultdict(int)
 for p in pos:
     idx[p] += 1
 
-min_cost = 1e16
-lo, hi = 0, len(pos)
-while lo <= hi:
-    mid = lo + ((hi - lo) // 2)
-    dest = pos[mid]
+
+def cost_to(dest):
     cost = 0
     for p, amount in idx.items():
         length = abs(p - dest)
         each_cost = int(length * (2 + cost_increment * (length - 1)) / 2)
         group_cost = amount * each_cost
         cost += group_cost
-    if cost < min_cost
-    min_cost = cost
+    return cost
 
-if cost > min_cost:
-    print(f'Destination {dest}\nCost {min_cost}')
-    exit()
+
+min_cost = 1e16
+lo, hi = 0, len(pos)
+delta = None
+
+while lo < hi:
+    mid = lo + ((hi - lo) // 2)
+    mid_left = mid  # looking for first distinct dest to the left
+    while pos[mid_left] == pos[mid]:
+        mid_left -= 1
+
+    mid_cost = cost_to(pos[mid])
+    if min_cost > mid_cost:
+        destination = pos[mid]
+        min_cost = mid_cost
+
+    delta = mid_cost - cost_to(pos[mid_left])
+    hi = mid if delta >= 0 else hi
+    lo = mid + 1 if delta < 0 else lo
+
+print(f'Destination {destination}\nCost {min_cost}')
 
