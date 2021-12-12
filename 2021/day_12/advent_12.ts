f9c9c6ce49771e32248bb115ef0ab8a09c3d7fc5
Alexander Isaev <isaevav27@gmail.com>
2021-12-12 08:07:25 +0100

    2021 Day 12 Part 2 TypeScript

--- 2021/day_12/advent_12.ts
+++ 2021/day_12/advent_12.ts
@@ -3,6 +3,8 @@
 const fs = require("fs");
 const lines = fs.readFileSync(0).toString().split('\n').filter((l: string) => !!l);;
 
+const debug = false;
+
 const map: {[ key: string]: Array<string>; } = {};
 lines
     .map((line: string) => line.split('-'))
@@ -15,21 +17,27 @@ lines
 
 interface String {
     isSmall(): boolean;
+    isTerminal(): boolean;
 }
 String.prototype.isSmall = function () { return this.toLowerCase() === this; }
+String.prototype.isTerminal  = function () { return this === 'start' || this === 'end'; }
 
 class Node {
     val: string;
     parent: Node | null;
     visited: Set<string>;
-    constructor(val: string, parent: Node | null, visited: Set<string>) {
+    visitedTwice: string | null;
+    constructor(val: string, parent: Node | null = null, visited: Set<string> = new Set(), visitedTwice: string | null = null) {
         this.val = val;
         this.parent = parent;
         this.visited = new Set(visited);
+        this.visitedTwice = visitedTwice;
         this.val.isSmall() && this.visited.add(this.val);
     }
 
-    createChild(val: string): Node { return new Node(val, this, this.visited); }
+    createChild(val: string, visitedTwice: boolean = false): Node {
+        return new Node(val, this, this.visited, visitedTwice ? val : this.visitedTwice);
+    }
 
     toString(): string {
         const backTrack: Array<string> = [];
@@ -44,21 +52,23 @@ class Node {
 
 const paths: Array<Node> = [];
 
-console.log(JSON.stringify(map, null, 2));
+debug && console.log(JSON.stringify(map, null, 2));
 
 function walk(v: Node) {
     paths.push(v);
     if (v.val === 'end') return;
-    console.log(`at ${v.val}, next up: ${map[v.val] || []}`);
-    for (let u of map[v.val] || []) {
+    debug && console.log(`at ${v.val}, next up: ${map[v.val] || []}`);
+    for (const u of map[v.val] || []) {
+        const canVisitTwice = !v.visitedTwice && u.isSmall() && !u.isTerminal();
         if (!v.visited.has(u)) walk(v.createChild(u));
+        else if (canVisitTwice) walk(v.createChild(u, true));
     }
 }
 
-walk(new Node('start', null, new Set()));
+walk(new Node('start'));
 
 const finishingPaths = paths.filter(p => p.val === 'end');
 
-finishingPaths.forEach(p => console.log(p.toString()));
+debug && finishingPaths.forEach(p => console.log(p.toString()));
 console.log(`There are ${finishingPaths.length} paths through the tunnels.`);
 
