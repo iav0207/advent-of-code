307f02d16bb6f6c231cef38a92a96bf2feb67641
Alexander Isaev <isaevav27@gmail.com>
2021-12-16 15:25:14 +0100

    2021 Refactoring + readme @ day 16

--- 2021/day_16/Advent16.kt
+++ 2021/day_16/Advent16.kt
@@ -4,12 +4,13 @@ import kotlin.math.max
 import kotlin.math.min
 
 
-const val DEBUG = false
-fun debug(a: Any) = if (DEBUG) println(a) else Unit
-fun debug(a: () -> Any) = if (DEBUG) println(a()) else Unit
+var debug = false
+fun debug(a: Any) = if (debug) println(a) else Unit
+fun debug(a: () -> Any) = if (debug) println(a()) else Unit
 
 
-fun main() {
+fun main(args: Array<String>) {
+    args.find { it == "-d" }?.also { debug = true }
     val bin: String = readLine()!!.toCharArray()
         .joinToString("") { it.toString().toInt(16).toString(2).padStart(4, '0') }
 
@@ -20,28 +21,29 @@ fun main() {
 }
 
 class Reader(private val binary: String, val c: Cursor = Cursor()) {
-    fun readInt(n: Int): Int = readBin(n).toInt(2)
-    fun read(n: Int): Long = readBin(n).toLong(2)
-    fun readBin(n: Int): String = binary.substring(c.value, c.value + n).also { c.advanceBy(n) }.also { printBin() }
+    fun readInt(n: Int): Int = readString(n).toInt(2)
+    fun readLong(n: Int): Long = readString(n).toLong(2)
+    fun readString(n: Int): String = binary.substring(c.value, c.advanceBy(n))
+        .also { if (left() == 0) printBinary() }
     fun left(): Int = binary.length - c.value
 
-    fun createChild(len: Int) = Reader(readBin(len))
+    fun createChild(len: Int) = Reader(readString(len))
 
-    fun printBin() = debug {
+    fun printBinary() = debug {
         with(StringBuilder()) {
             var pr = 0
             c.chunks.forEach { append(binary.substring(pr, pr + it)).append(' '); pr += it }
             append(binary.substring(pr)).toString()
         }
     }
-}
 
-class Cursor(var value: Int = 0, val chunks: MutableList<Int> = mutableListOf()) {
-    fun advanceBy(n: Int) = chunks.add(n).also { value += n }
+    class Cursor(var value: Int = 0, val chunks: MutableList<Int> = mutableListOf()) {
+        fun advanceBy(n: Int): Int = chunks.add(n).also { value += n }.let { value }
+    }
 }
 
 class Packet(val r: Reader) {
-    val version = r.read(3)
+    val version = r.readInt(3)
     val typeId = r.readInt(3)
     val isLiteral = typeId == 4
     val label = if (isLiteral) "LITERAL" else "OPERATOR"
@@ -51,19 +53,18 @@ class Packet(val r: Reader) {
     val nested = if (isLiteral) listOf() else readSubPackets()
     val value: Long? = if (isLiteral) readLiteral() else null
 
-    fun versionsSum(): Long = version + nested.sumOf { it.versionsSum() }
+    fun versionsSum(): Int = version + nested.sumOf { it.versionsSum() }
 
     fun evaluate(): Long = value ?: run {
         if (nested.size > 1) debug { "Evaluate op=$typeId on ${nested.map { it.evaluate() }}" }
         nested.fold(-1L) { acc, b -> if (acc < 0) b.evaluate() else operation(acc, b.evaluate()) }
     }
 
-    val operation: (Long, Long) -> Long = when(typeId) {
+    val operation: (Long, Long) -> Long get() = when(typeId) {
         0 -> { a, b -> a + b }
         1 -> { a, b -> a * b }
         2 -> { a, b -> min(a, b) }
         3 -> { a, b -> max(a, b) }
-        4 -> { _, _ -> error("invoking operation on a literal?") }
         5 -> { a, b -> if (a > b) 1 else 0  }
         6 -> { a, b -> if (a < b) 1 else 0  }
         7 -> { a, b -> if (a == b) 1 else 0  }
@@ -90,11 +91,11 @@ class Packet(val r: Reader) {
     }
 
     fun readLiteral(): Long = with(StringBuilder()) {
-        var read: Boolean
+        var readLong: Boolean
         do {
-            read = r.read(1) == 1L
-            append(r.readBin(4))
-        } while (read)
+            readLong = r.readInt(1) == 1
+            append(r.readString(4))
+        } while (readLong)
         toString()
             .also { debug { "Read literal in binary: $it (${it.toLong(2)})" } }
             .toLong(2)
---
+++ 2021/day_16/README.md
@@ -0,0 +1,17 @@
+Spec https://adventofcode.com/2021/day/16
+
+Execution
+
+```bash
+make < input.txt
+```
+
+Output
+
+```
+Version numbers total is 917
+Expression evaluates to 2536453523344
+```
+
+[Debug logs](https://gist.github.com/iav0207/f9a218b6e7348804a3f54abab50ead24) :)
+
