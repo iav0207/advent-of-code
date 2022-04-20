4459d83f33342b1bc95a8e78a36dd5a4892962e0
Alexander Isaev <isaevav27@gmail.com>
2022-04-20 18:15:32 +0200

    2020 Day 1 Refactoring

---
+++ 2020/day_01/README.md
@@ -0,0 +1,17 @@
+Spec https://adventofcode.com/2020/day/1
+
+Two- and three-sums.
+
+Execution
+
+```bash
+./advent_01.py < input
+```
+
+Output
+
+```
+422 * 577 * 1021 = 248607374
+543 * 1477 = 802011
+```
+
--- 2020/day_01/advent_01.py
+++ 2020/day_01/advent_01.py
@@ -7,12 +7,6 @@ lines = sys.stdin.readlines()
 values = sorted([int(line[:-1]) for line in lines])
 values_set = set(values)
 
-idx = {v: i for i, v in enumerate(values)}
-i = 0
-for v in range(0, 2020):
-    i = idx.get(v, i)
-    idx[v] = i
-
 printed = set()
 
 def answer(*values):
@@ -28,8 +22,9 @@ for first in values:
     if second > 0 and second in values_set:
         answer(first, second)
     # part 2
-    for i in range(idx[2020 - first] - 1, -1, -1):
-        second = values[i]
+    for second in values:
+        if second > 2020 - first:
+            break
         third = 2020 - first - second
         if third > 0 and third in values_set:
             answer(first, second, third)
