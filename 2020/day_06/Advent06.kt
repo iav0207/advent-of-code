2999b6b965b830a3526c930417dc4b84ba0b3459
Alexander Isaev <isaevav27@gmail.com>
2022-04-23 15:46:49 +0200

    2020 Day 6 Refactoring? Not sure which version is better

--- 2020/day_06/Advent06.kt
+++ 2020/day_06/Advent06.kt
@@ -1,18 +1,25 @@
 package advent2020.day06
 
 fun main() {
-    val groups: List<Group> = generateSequence { readlnOrNull() }
-        .joinToString("\n")
-        .split("\n\n")
-        .map { it.split("\n") }
+    val groups: List<Group> = generateSequence { readGroup() }.toList()
 
     val anyYesSum = groups.sumOf { it.countAnyYesQuestions() }
     val allYesSum = groups.sumOf { it.countAllYesQuestions() }
+
     println("The sum of group counts is: any=$anyYesSum all=$allYesSum")
 }
 
-typealias Group = List<String>
+typealias Answers = Set<Char>
+typealias Group = List<Answers>
 
-fun Group.countAnyYesQuestions(): Int = flatMap { it.toSet() }.toSet().size
-fun Group.countAllYesQuestions(): Int = map { it.toSet() }.reduce { acc, it -> acc.intersect(it) }.size
+fun readGroup(): Group? = generateSequence { readAnswersOrNull() }
+    .toList()
+    .takeUnless { it.isEmpty() }
+
+fun readAnswersOrNull(): Answers? = readlnOrNull()
+    ?.takeUnless { it.isEmpty() }
+    ?.toSet()
+
+fun Group.countAnyYesQuestions(): Int = flatMap { it }.toSet().size
+fun Group.countAllYesQuestions(): Int = reduce { acc, it -> acc.intersect(it) }.size
 
