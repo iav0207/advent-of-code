4ad4d874d0b5bf0ca22a9674f5a47f89ea19f702
Alexander Isaev <isaevav27@gmail.com>
2022-04-23 13:25:53 +0200

    Kotlin stdlib installation script refactoring

--- scripts/install_kotlin_stdlib.sh
+++ scripts/install_kotlin_stdlib.sh
@@ -4,8 +4,6 @@ scripts_dir="$(dirname $0)"
 project_root=$scripts_dir/..
 lib_dir=$project_root/lib
 
-mkdir -p $lib_dir
-
 function pkg_url() {
     group=$1 artifact=$2
     echo "https://repo1.maven.org/maven2/${group//.//}/${artifact}"
@@ -22,21 +20,31 @@ function download_jar() {
     jar_url="$(pkg_url $group $artifact)/${version}/${jar_name}"
 
     echo Downloading $artifact version $version
+    mkdir -p $lib_dir
     curl -s -o $lib_dir/$jar_name $jar_url
 }
 
+function indent() { while read line; do echo "    $line"; done; }
+
+function link_kt_source_dirs_to_lib() {
+    kt_src_dirs=$(dirname $(find $project_root -iname "*.kt") | sort -u)
+    for kt_src_dir in $kt_src_dirs; do
+        lib_link=$kt_src_dir/lib
+        if [[ -d $lib_link ]] || [[ -L $lib_link ]]; then
+            printf "\n${lib_link} already exists:\n"
+            ls -l $lib_link | indent
+        else
+            echo "Linking $lib_link -> $lib_dir"
+            ln -s $lib_dir $kt_src_dir/lib
+        fi
+    done
+}
+
+###
+###    Script
+###
+
 download_jar org.jetbrains.kotlin kotlin-stdlib 1.6.21
 
-for kt_src_dir in $(dirname $(find $project_root -iname "*.kt") | sort -u); do
-    lib_link=$kt_src_dir/lib
-    if [[ -d $lib_link ]] || [[ -L $lib_link ]]; then
-        echo
-        echo $lib_link already exists:
-        while read line; do echo "    $line"; done < <(ls -l $lib_link)
-        echo
-    else
-        echo "Linking $lib_link -> $lib_dir"
-        ln -s $lib_dir $kt_src_dir/lib
-    fi
-done
+link_kt_source_dirs_to_lib
 
