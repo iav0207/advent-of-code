0085eec888f3389657b8438f8425a5d452a68929
Alexander Isaev <isaevav27@gmail.com>
2022-11-14 18:36:02 +0100

    2020 Day 19 stash

--- 2020/day_19/Advent19.kt
+++ 2020/day_19/Advent19.kt
@@ -1,5 +1,8 @@
 package advent2020.day19
 
+import java.util.ArrayDeque
+import java.util.Deque
+
 var debug = false
 fun debug(a: () -> Any): Unit = if (debug) println(a()) else Unit
 
@@ -13,19 +16,11 @@ fun main(vararg args: String) {
 
     val unparsedRules: Map<String, String> = input.filter { ":" in it }
         .associate { it.substringBefore(":") to it.substringAfter(": ") }
+        .toMutableMap()
 
-    val rules: MutableMap<String, Rule> = mutableMapOf()
+    val rules = parse(unparsedRules)
 
-    fun parseRule(id: String): Rule {
-        if (id in rules) return rules[id]!!
-        val ruleStr = unparsedRules[id] ?: id // !!!
-        return when {
-            "\"" in ruleStr -> SimpleRule(id, ruleStr[1])
-            "|" in ruleStr -> OrRule(id, ruleStr.split(" | ").map { parseRule(it) })
-            else -> SeqRule(id, ruleStr.split(" ").map { parseRule(it) })
-        }.also { if (" " !in id) rules.put(id, it) }
-    }
-    val rule0 = parseRule("0")
+    val rule0 = rules["0"]!!
     debug { rules }
 
     val messages = input.filter { ":" !in it && it.isNotEmpty() }
@@ -36,6 +31,41 @@ fun main(vararg args: String) {
     }
 
     print("Part 1: $matchCount")
+}
+
+fun parse(unparsed: Map<String, String>): Map<String, Rule> {
+    val parsed: MutableMap<String, Rule> = mutableMapOf()
+    val ruleGetter: (ID) -> Rule = { parsed[it]!! }
+
+    fun parseRule(id: String): Rule {
+        if (id in parsed) return parsed[id]!!
+        val ruleStr = unparsed[id] ?: id // !!!
+        debug { "Parsing $ruleStr" }
+        return when {
+            "\"" in ruleStr -> SimpleRule(id, ruleStr[1])
+            "|" in ruleStr -> {
+                ruleStr.split(" | ")
+                    .filter { " " in it }
+                    .forEach { compositeId -> // ephemeral "composite ids" like `14 2`
+                        parsed[compositeId] =
+                            SeqRule(compositeId, compositeId.split(" "), ruleGetter)
+                    }
+                OrRule(id, ruleStr.split(" | "), ruleGetter)
+            }
+            else -> SeqRule(id, ruleStr.split(" "), ruleGetter)
+        }
+    }
+
+    val toParse: Deque<ID> = unparsed.keys.toCollection(ArrayDeque())
+    while (toParse.isNotEmpty()) {
+        val curr = toParse.pop()
+        try {
+            parsed[curr] = parseRule(curr)
+        } catch (ex: NullPointerException) {
+            toParse.add(curr)
+        }
+    }
+    return parsed
 }
 
 interface Rule {
@@ -54,9 +84,13 @@ class SimpleRule(override val id: String, private val char: Char) : Rule {
     override fun toString() = "SimpleRule $id"
 }
 
-class OrRule(override val id: String, private val subrules: Collection<Rule>) : Rule {
+class OrRule(override val id: String,
+             private val subrules: Collection<ID>,
+             private val getRule: (ID) -> Rule,
+) : Rule {
     override fun matchAndAdvance(str: String): Sequence<Int> {
         return subrules.asSequence()
+            .map(getRule)
             .flatMap { it.matchAndAdvance(str) }
             .distinct()
             .onEach { debug { "OrRule $id matches '$str', advance $it" } }
@@ -65,7 +99,10 @@ class OrRule(override val id: String, private val subrules: Collection<Rule>) : 
     override fun toString() = "OrRule $id"
 }
 
-class SeqRule(override val id: String, private val subrules: List<Rule>) : Rule {
+class SeqRule(override val id: String,
+              private val subrules: List<ID>,
+              private val getRule: (ID) -> Rule,
+) : Rule {
     override fun matchAndAdvance(str: String): Sequence<Int> {
         debug { "Matching SeqRule $id against '$str'" }
         return matchAndAdvance(str, 0, 0).distinct()
@@ -74,9 +111,12 @@ class SeqRule(override val id: String, private val subrules: List<Rule>) : Rule 
     private fun matchAndAdvance(str: String, ruleNum: Int, cursor: Int) : Sequence<Int> {
         if (cursor > str.length) return sequenceOf()
         if (ruleNum in subrules.indices) {
-            return subrules[ruleNum].matchAndAdvance(str.substring(cursor)).flatMap { advance ->
-                matchAndAdvance(str, ruleNum + 1, cursor + advance)
-            }.distinct().onEach { debug { "SeqRule $id matches '$str', advance $it " } }
+            return subrules[ruleNum]
+                .let(getRule)
+                .matchAndAdvance(str.substring(cursor))
+                .flatMap { advance -> matchAndAdvance(str, ruleNum + 1, cursor + advance) }
+                .distinct()
+                .onEach { debug { "SeqRule $id matches '$str', advance $it " } }
         }
         return sequenceOf(cursor)
     }
