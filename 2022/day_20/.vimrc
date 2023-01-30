21905c0bac28c420a1f464c410c93a95b11c0adc
Alexander Isaev <isaevav27@gmail.com>
2023-01-30 17:15:23 +0100

    2022 Day 20 another attempt

--- 2022/day_20/.vimrc
+++ 2022/day_20/.vimrc
@@ -1,4 +1,5 @@
 map <F5> :!clear<CR>
+map <F6> <F5>:!make debug < example.txt<CR>
 map <F7> <F5>:!make run < input.txt<CR>
 map <F8> <F5>:!make debug < input.txt<CR>
 
--- 2022/day_20/Advent20.kts
+++ 2022/day_20/Advent20.kts
@@ -5,60 +5,87 @@ fun debug(a: () -> Any): Unit = if (debug) println(a()) else Unit
 
 debug = "-d" in args
 
-data class Payload(val id: Int, val value: Long)
+// items are not unique
 
-class Node(
-    var payload: Payload,
-    var prev: Node? = null,
-    var next: Node? = null,
-) {
-    fun prev(): Node = prev!!
-    fun next(): Node = next!!
-    fun swapWith(other: Node): Node {
-        val otherPayload = other.payload
-        other.payload = payload
-        payload = otherPayload
-        return other
+data class Item(val id: Int, val value: Long)
+
+val initialArrangement: List<Item> = generateSequence { readLine()?.trimEnd()?.toLong() }
+    .withIndex()
+    .map { (i, it) -> Item(i, it) }
+    .toList()
+
+val len = initialArrangement.size
+// TODO better names for cycle and cycLen
+val cycle = len - 1 // that's because we carry the item over len-1 remaining positions
+
+val mix = initialArrangement.toMutableList()
+
+fun cyclic(n: Int, cycLen: Int = cycle): Int {
+    var c = n
+    while (c < 0) c += cycLen
+    c %= cycLen
+    check(c >= 0)
+    return c
+}
+
+fun <T> MutableList<T>.swap(i: Int, j: Int) {
+    val ai = get(i)
+    val aj = get(j)
+    set(i, aj)
+    set(j, ai)
+}
+
+fun MutableList<Item>.move(id: Int) {
+    val from: Int = withIndex().find { (_, item) -> item.id == id }!!.index
+    val value = get(from).value
+    check(value == initialArrangement[id].value)
+    val to = cyclic(from + value.toInt(), cycLen = len)
+    debug { "Moving $value from $from to $to" }
+    if (to == from) return
+    val direc = if (value >= 0) 1 else -1
+    var (prev, curr) = Pair(from, cyclic(from + direc, cycLen = len))
+    debug { "direc $direc" }
+    while (true) {
+        debug { "swap $prev <> $curr" }
+        check(prev != curr)
+        swap(prev, curr)
+        if (curr == to) break
+        prev = curr
+        curr = cyclic(curr + direc, cycLen = len)
     }
-    fun asSequence(): Sequence<Node> = generateSequence(this) { it.next() }
 }
 
-val input: List<Long> = generateSequence { readLine()?.trimEnd()?.toLong() }.toList()
-val len = input.size
+val expected = listOf(
+    listOf(1, 2, -3, 3, -2, 0, 4),
+    listOf(2, 1, -3, 3, -2, 0, 4),
+    listOf(1, -3, 2, 3, -2, 0, 4),
+    listOf(1, 2, 3, -2, -3, 0, 4),
+    listOf(1, 2, -2, -3, 0, 3, 4),
+    listOf(1, 2, -3, 0, 3, 4, -2),
+    listOf(1, 2, -3, 0, 3, 4, -2),
+    listOf(1, 2, -3, 4, 0, 3, -2),
+).map { sublist -> sublist.map { it -> it.toLong() } }
 
-fun buildRing(): Collection<Node> {
-    val ring = input.withIndex()
-        .map { (i, it) -> Payload(i, it) }
-        .map { Node(it) }
-    ring.zipWithNext().forEach { (prev, next) ->
-        prev.next = next
-        next.prev = prev
-    }
-    ring.first().prev = ring.last()
-    ring.last().next = ring.first()
-    return ring
+debug { "Initial:\t${initialArrangement.map { it.value }}" }
+for (id in initialArrangement.indices) {
+    println(id)
+    // val actual: List<Long> = mix.map { it.value }
+    // val shExpected = expected[id].toMutableList()
+    // while (shExpected[0] != actual[0]) shExpected.add(shExpected.removeAt(0))
+    // check(shExpected == actual) { "\nid: $id\nExpected:\t${expected[id]}\nActual:\t\t$actual" }
+    mix.move(id)
+    debug { "Moved ${initialArrangement[id].value}:\t${mix.map { it.value }}" }
 }
 
-val mix = buildRing()
+val indexOfZero = mix.withIndex()
+    .find { (_, item) -> item.value == 0L }!!
+    .index
 
-debug { mix.first().asSequence().take(len).map { it.payload.value }.toList() }
+val p1 = listOf(1000, 2000, 3000)
+    .map { indexOfZero + it }
+    .map { mix[it % len].value }
+    .also { debug { "summing $it" } }
+    .sum()
 
-for (id in 0 until len) {
-    val start = mix.find { node -> node.payload.id == id }!!
-    val num = start.payload.value
-    fun step(node: Node) = if (num >= 0L) node.swapWith(node.next()) else node.swapWith(node.prev())
-    var toGo = Math.abs(num)
-    var node = start
-    while (toGo-- > 0) node = step(node)
-    debug { mix.first().asSequence().take(len).map { it.payload.value }.toList() }
-}
-
-val zeroNode = mix.find { it.payload.value == 0L }!!
-var p1 = 0L
-var cur = zeroNode
-for (i in 1..3000) {
-    cur = cur.next()
-    if (i % 1000 == 0) p1 += cur.payload.value
-}
 println("Part 1: $p1")
 
