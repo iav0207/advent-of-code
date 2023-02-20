5c537c8c7f1a3824feb3aaffca701542780c289e
Alexander Isaev <isaevav27@gmail.com>
2023-02-20 14:28:29 +0100

    2022 Day 22 Part 1 Refactoring

--- 2022/day_22/advent_22.py
+++ 2022/day_22/advent_22.py
@@ -2,6 +2,8 @@
 
 import sys
 import re
+import math
+from itertools import product
 
 debug_mode = '-d' in sys.argv
 
@@ -52,27 +54,41 @@ def at(row, col):
         return ' '
 
 
-def move(instruction):
-    facing, (row, col) = state
-    turn, moves = instruction
-    moves = int(moves)
+def rotate(facing, turn_r_or_l):
     facing_idx = direc.index(facing)
-    facing_idx += 1 if turn == 'R' else -1
+    facing_idx += 1 if  turn_r_or_l == 'R' else -1
     while facing_idx < 0:
         facing_idx += len(direc)
     facing_idx %= len(direc)
-    facing = direc[facing_idx]
-    for i in range(moves):
-        inc = next_inc[facing_idx]
-        nxt = row + inc[0], col + inc[1]
+    return direc[facing_idx]
+
+
+coord_increment = lambda facing: next_inc[direc.index(facing)]
+add_vec = lambda a, b: tuple([ai + bi for ai, bi in zip(a, b)])
+"""Element-wise product of vectors. Not a vector product."""
+times_vec = lambda a, b: tuple([ai * bi for ai, bi in zip(a, b)])
+
+
+def wrap(coord, facing):
+    debug('flat wrap!')
+    inc = coord_increment(facing)
+    dec = times_vec((-1, -1), inc)
+    cursor = coord
+    while at(*cursor) != ' ':  # crawling back until we hit the void
+        cursor = add_vec(cursor, dec)
+    cursor = add_vec(cursor, inc)  # take one step forward into the field
+    return cursor
+
+
+def move(instruction):
+    facing, (row, col) = state
+    turn, moves = instruction
+    facing = rotate(facing, turn)
+    for i in range(int(moves)):
+        inc = coord_increment(facing)
+        nxt = add_vec((row, col), inc)
         if at(*nxt) == ' ':
-            debug('wrap!')
-            cursor = row, col
-            while at(*cursor) != ' ':
-                cursor = cursor[0] - inc[0], cursor[1] - inc[1]
-            cursor = cursor[0] + inc[0], cursor[1] + inc[1]
-            debug(cursor)
-            nxt = cursor
+            nxt = wrap((row, col), facing)
         if at(*nxt) == '#':
             break
         row, col = nxt
@@ -86,4 +102,19 @@ debug(f'row {row+1} col {col+1} fac {direc.index(facing)}')
 result = 1000 * (row + 1) + 4 * (col + 1) + direc.index(facing)
 
 print(f'Part 1: {result}')
+
+# Part 2 plan: 1. find the face 2. apply coord transformations depending on the path to the face
+# face rotations:
+# starting face, always : (1, 0, 0)
+rota = {
+    'R': (0, 1, 0),
+    'D': (0, 0, -1),
+    'L': (0, -1, 0),
+    'U': (0, 0, 1),
+}
+
+# edge length
+total_surface = sum(1 if at(row, col) != ' ' else 0 for row, col in product(range(nrows), range(ncols)))
+edge_length = int(math.sqrt(total_surface / 6))
+debug(f'edge length is {edge_length}')
 
