42948a2b80005ce7049002bb458da1637f466795
Alexander Isaev <IsaevAV27@gmail.com>
2023-12-02 09:50:14 +0100

    2023 Day 2 Part 2

--- 2023/day_02/Advent02.kt
+++ 2023/day_02/Advent02.kt
@@ -14,10 +14,9 @@ fun main(vararg args: String) {
         .map { (gameId, revealsStrs) -> Game(gameId, revealsStrs.map { it.toReveal() }) }
 
     val check = Bag(r = 12, g = 13, b = 14)
-    val part1 = games.filter { it.isPossibleWith(check) }
-        .onEach { debug { "possible: $it" } }
-        .sumOf { it.id }
-    println("Part 1 : ${part1}")
+    val part1 = games.filter { it.isPossibleWith(check) }.sumOf { it.id }
+    println("Part 1 : $part1")
+    println("Part 2 : ${games.sumOf { it.minimumBag().power() }}")
 }
 
 val numbersRegex = "\\d+".toRegex()
@@ -34,4 +33,13 @@ data class Game(val id: Int, val reveals: List<Reveal>)
 
 fun Game.isPossibleWith(bag: Bag): Boolean =
     reveals.all { it.r <= bag.r && it.g <= bag.g && it.b <= bag.b }
+
+fun Game.minimumBag(): Bag =
+    reveals.fold(Bag(0, 0, 0)) { acc, it -> Bag(
+        r = maxOf(acc.r, it.r),
+        g = maxOf(acc.g, it.g),
+        b = maxOf(acc.b, it.b),
+    ) }
+
+fun Bag.power(): Long = r.toLong() * b * g
 
