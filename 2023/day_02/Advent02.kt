a6c6087e2562096758024f956726e09aab1f9862
Alexander Isaev <IsaevAV27@gmail.com>
2023-12-02 09:59:18 +0100

    2023 Day 2 Refactoring

--- 2023/day_02/Advent02.kt
+++ 2023/day_02/Advent02.kt
@@ -10,36 +10,34 @@ fun main(vararg args: String) {
     debug = "-d" in args
     val input = generateSequence { readLine()?.trimEnd() }.toList()
     val games = input.map { it.split(": ") }
-        .map { (gameStr, revealsStr) -> numbersRegex.find(gameStr)!!.value.toInt() to revealsStr.split("; ") }
+        .map { (gameStr, revealsStr) -> gameStr.findNumber() to revealsStr.split("; ") }
         .map { (gameId, revealsStrs) -> Game(gameId, revealsStrs.map { it.toReveal() }) }
 
-    val check = Bag(r = 12, g = 13, b = 14)
-    val part1 = games.filter { it.isPossibleWith(check) }.sumOf { it.id }
-    println("Part 1 : $part1")
+    val bag = Bag(r = 12, g = 13, b = 14)
+    println("Part 1 : ${games.filter { it.isPossibleWith(bag) }.sumOf { it.id }}")
     println("Part 2 : ${games.sumOf { it.minimumBag().power() }}")
 }
 
+fun String.findNumber(): Int = numbersRegex.find(this)!!.value.toInt()
 val numbersRegex = "\\d+".toRegex()
 
 fun String.toReveal() = split(", ")
-    .associate { it.split(" ").let { words -> words[1] to words[0].toInt() } }
+    .associate { it.split(" ").let { (qty, color) -> color to qty.toInt() } }
     .run { Reveal(cubes("red"), cubes("green"), cubes("blue")) }
 
 fun Map<String, Int>.cubes(color: String): Int = get(color) ?: 0
 
-data class Reveal(val r: Int, val g: Int, val b: Int)
-data class Bag(val r: Int, val g: Int, val b: Int)
+data class CubeSet(val r: Int, val g: Int, val b: Int)
 data class Game(val id: Int, val reveals: List<Reveal>)
+typealias Bag = CubeSet
+typealias Reveal = CubeSet
 
 fun Game.isPossibleWith(bag: Bag): Boolean =
     reveals.all { it.r <= bag.r && it.g <= bag.g && it.b <= bag.b }
 
-fun Game.minimumBag(): Bag =
-    reveals.fold(Bag(0, 0, 0)) { acc, it -> Bag(
-        r = maxOf(acc.r, it.r),
-        g = maxOf(acc.g, it.g),
-        b = maxOf(acc.b, it.b),
-    ) }
+fun Game.minimumBag(): Bag = reveals.fold(Bag(0, 0, 0)) { acc, it ->
+    Bag(r = maxOf(acc.r, it.r), g = maxOf(acc.g, it.g), b = maxOf(acc.b, it.b))
+}
 
 fun Bag.power(): Long = r.toLong() * b * g
 
