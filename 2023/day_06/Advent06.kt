dc1b917d1c474719bf7ff19a90f27c1704c12113
Alexander Isaev <IsaevAV27@gmail.com>
2023-12-06 06:30:16 +0100

    2023 Day 6 Part 1

--- 2023/day_06/Advent06.kt
+++ 2023/day_06/Advent06.kt
@@ -8,17 +8,17 @@ fun <T : Any> T.debug(a: (T) -> Any = { this }): T = also { if (debug) println(a
 
 fun main(vararg args: String) {
     debug = "-d" in args
-    val records = generateSequence { readLine()?.trimEnd() }.toList().debug()
-        .map { it.split("\\s+".toRegex()).drop(1).map { v -> v.toInt() } }
+    val input = generateSequence { readLine()?.trimEnd() }.toList().debug()
+
+    val records = input
+        .map { it.split("\\s+".toRegex()).drop(1).map { v -> v.toLong() } }
         .let { (times, distances) -> times.indices.map { i -> Run(times[i], distances[i]) } }
         .debug()
-    for (t in 0..records.minOf { it.time }) {
-        
-    }
+
     records
         .map { r ->
             generateSequence(1) { it + 1 }
-                .take(r.time)
+                .take(r.time.toInt())
                 .count { releaseTime ->
                     val timeLeft = r.time - releaseTime
                     val speed = releaseTime
@@ -28,7 +28,19 @@ fun main(vararg args: String) {
         }
         .fold(1) { acc, it -> acc * it }
         .also { println("Part 1: $it") }
+
+    val record = input.map { "\\d+".toRegex().findAll(it).map { it.value }.joinToString("").toLong() }
+        .let { (time, dist) -> Run(time, dist) }
+
+    generateSequence(1L) { it + 1 }
+        .take(record.time.toInt())
+        .count { releaseTime ->
+            val timeLeft = record.time - releaseTime
+            val speed = releaseTime
+            val distance = timeLeft * speed
+            distance > record.distance
+        }.also { println("Part 2: $it") }
 }
 
-data class Run(val time: Int, val distance: Int)
+data class Run(val time: Long, val distance: Long)
 
