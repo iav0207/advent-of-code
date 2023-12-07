96abdf857ff359cec24e313e36b8a39baf8ad16f
Alexander Isaev <IsaevAV27@gmail.com>
2023-12-07 09:33:02 +0100

    2023 Day 7 Part 2

--- 2023/day_07/Advent07.kt
+++ 2023/day_07/Advent07.kt
@@ -11,44 +11,64 @@ fun main(vararg args: String) {
     val hands = generateSequence { readLine()?.trimEnd() }.toList()
         .map { it.split(" ").run { Hand(get(0), get(1).toInt()) } }
 
-    val totalWinnings = hands.sortedWith(order).debug()
-        .withIndex()
-        .map { (i, it) -> (i + 1) * it.bid }.debug()
-        .sum()
-    println("Part 1: $totalWinnings")
+    (1..2).forEach { part ->
+        val order = Solution(withJokers = part > 1).order
+        var totalWinnings = hands.sortedWith(order).debug()
+            .withIndex()
+            .map { (i, it) -> (i + 1) * it.bid }.debug()
+            .sum()
+        println("Part $part: $totalWinnings")
+    }
 }
 
-data class Hand(val cards: String, val bid: Int) {
-    val type: Int = typeChecks.indexOfFirst { cards.it() }
+data class Hand(val cards: String, val bid: Int)
+
+class Solution(val withJokers: Boolean = false) {
+    val order: Comparator<Hand> = compareBy<Hand> { it.type }
+        .then({ a: Hand, b: Hand -> compareCards(a.cards, b.cards) })
+        .reversed()
+
+    fun compareCards(le: String, ri: String): Int {
+        return le.zip(ri).map { (ele, eri) -> compareCards().compare(ele, eri) }.firstOrNull { it != 0 } ?: 0
+    }
+    fun compareCards(): Comparator<Char> = compareBy { cardValuePrecedence.indexOf(it) }
+    val cardValuePrecedence = if (withJokers) "AKQT98765432J" else "AKQJT98765432"
+
+    val Hand.type: Int get() = typeChecks.indexOfFirst { cards.it() }
+
+    val typeChecks: List<String.() -> Boolean> = listOf(
+        { isFiveOfAKind() },
+        { isFourOfAKind() },
+        { isFullHouse() },
+        { isThreeOfAKind() },
+        { isTwoPair() },
+        { isOnePair() },
+        { isHighCard() },
+    )
+
+    fun String.isFiveOfAKind(): Boolean = groups().size == 1
+    fun String.isFourOfAKind(): Boolean = groups().values.any { it == 4 }
+    fun String.isFullHouse(): Boolean = counts() == listOf(2, 3)
+    fun String.isThreeOfAKind(): Boolean = counts() == listOf(1, 1, 3)
+    fun String.isTwoPair(): Boolean = counts() == listOf(1, 2, 2)
+    fun String.isOnePair(): Boolean = counts() == listOf(1, 1, 1, 2)
+    fun String.isHighCard(): Boolean = groups().size == 5
+    fun String.counts(): List<Int> = groups().values.sorted()
+    fun String.groups(): Map<Char, Int> = groupingBy { it }.eachCount().run {
+        if (!withJokers) return this
+        val groupsExceptJoker = filterKeys { k -> k != 'J' }.toList().sortedBy { it.second }
+        val jokersCount = get('J') ?: 0
+        val bestGroup = groupsExceptJoker.firstOrNull() ?: 'A' to 0
+        val bestGroupWithJoker = bestGroup.run { first to second + jokersCount }
+            .debug { "group with joker: $it" }
+        listOf(bestGroupWithJoker)
+            .plus(groupsExceptJoker.drop(1))
+            .toMap()
+            .debug { "before: $this\nafter: $it" }
+        // Part 2 passes on the example, but the answer on the real input is not accepted
+        // 252634690 is too high
+        // 249002740 is too low
+        // 250002740 is too high
+    }
 }
-
-val order: Comparator<Hand> = compareBy<Hand> { it.type }
-    .then({ a: Hand, b: Hand -> compareCards(a.cards, b.cards) })
-    .reversed()
-
-fun compareCards(le: String, ri: String): Int {
-    return le.zip(ri).map { (ele, eri) -> compareCards().compare(ele, eri) }.firstOrNull { it != 0 } ?: 0
-}
-fun compareCards(): Comparator<Char> = compareBy { cardValuePrecedence.indexOf(it) }
-val cardValuePrecedence = "AKQJT98765432"
-
-val typeChecks: List<String.() -> Boolean> = listOf(
-    { isFiveOfAKind() },
-    { isFourOfAKind() },
-    { isFullHouse() },
-    { isThreeOfAKind() },
-    { isTwoPair() },
-    { isOnePair() },
-    { isHighCard() },
-)
-
-fun String.isFiveOfAKind(): Boolean = groups().size == 1
-fun String.isFourOfAKind(): Boolean = groups().values.any { it == 4 }
-fun String.isFullHouse(): Boolean = counts() == listOf(2, 3)
-fun String.isThreeOfAKind(): Boolean = counts() == listOf(1, 1, 3)
-fun String.isTwoPair(): Boolean = counts() == listOf(1, 2, 2)
-fun String.isOnePair(): Boolean = counts() == listOf(1, 1, 1, 2)
-fun String.isHighCard(): Boolean = groups().size == 5
-fun String.counts(): List<Int> = groups().values.sorted()
-fun String.groups(): Map<Char, Int> = groupingBy { it }.eachCount()
 
--- 2023/day_07/README.md
+++ 2023/day_07/README.md
@@ -1,6 +1,8 @@
 Spec https://adventofcode.com/2023/day/7
 
-Desc Forgot to write a readme in 2023/day_07
+Desc Playing Camel poker. Comparing hands of cards, applying Joker as a wildcard to maximize hands' values.
+
+Solution for part 2 passes on the example, but the answer on the real input is not accepted 🤷.
 
 Execution
 
@@ -11,5 +13,7 @@ make < input.txt
 Output
 
 ```
+Part 1: 248105065
+Part 2: 249002740
 ```
 
