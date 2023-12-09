6194d17e041efbc5e0775d76e53935b05a01bc82
Alexander Isaev <IsaevAV27@gmail.com>
2023-12-09 07:06:37 +0100

    2023 Day 9 Part 2

--- 2023/day_09/Advent09.kt
+++ 2023/day_09/Advent09.kt
@@ -13,6 +13,7 @@ fun main(vararg args: String) {
         .toList()
 
     println("Part 1: ${Solution(input).part1()}")
+    println("Part 2: ${Solution(input).part2()}")
 }
 
 class Solution(val input: List<MutableList<Long>>) {
@@ -20,22 +21,34 @@ class Solution(val input: List<MutableList<Long>>) {
     val m = input[0].size
 
     fun part1(): Long = input.indices.sumOf { calcNext(it) }
+    fun part2(): Long = input.indices.sumOf { calcPrev(it).debug { "LEFT $it" } }
+
+    fun calcNext(i: Int): Long = diffTable(i).debug { "DT[$i] = $it" }.extrapolateRight()
+    fun calcPrev(i: Int): Long = diffTable(i).extrapolateLeft()
 
     fun diffTable(i: Int): DiffTable =
-        generateSequence(input[i].debug()) { .zipWithNext { prv, nxt -> nxt - prv }.toMutableList().debug() }
+        generateSequence(input[i].debug()) { it.zipWithNext { prv, nxt -> nxt - prv }.toMutableList().debug() }
             .takeWhile { row -> !row.all { it == 0L } } // won't return trailing row of zeros
             .toList()
 
-    fun DiffTable.extrapolate(): Long = extrapolate(0, m)
+    fun DiffTable.extrapolateRight(): Long = extrapolateRight(0, m)
+    fun DiffTable.extrapolateLeft(): Long = extrapolateLeft(0, -1)
 
-    fun DiffTable.extrapolate(i: Int, j: Int): Long {
+    fun DiffTable.extrapolateLeft(i: Int, j: Int): Long = run {
+        debug { "extr $i $j" }
+        when {
+            i == size -> 0L
+            i in indices && j in get(i).indices -> get(i).get(j)
+            else -> extrapolateLeft(i, j + 1) - extrapolateLeft(i + 1, j)
+        }
+    }
+
+    fun DiffTable.extrapolateRight(i: Int, j: Int): Long {
         debug { "extr $i $j" }
         if (i == size) return 0L
         if (i in indices && j in get(i).indices) return get(i).get(j)
-        return extrapolate(i, j - 1) + extrapolate(i + 1, j - 1)
+        return extrapolateRight(i, j - 1) + extrapolateRight(i + 1, j - 1)
     }
-
-    fun calcNext(i: Int): Long = diffTable(i).debug { "DT[$i] = $it" }.extrapolate()
 }
 
 typealias DiffTable = List<MutableList<Long>>
