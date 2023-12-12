8606e0b9faea89d539e1258f09545ae18444d84c
Alexander Isaev <IsaevAV27@gmail.com>
2023-12-12 21:03:19 +0100

    2023 Day 12 Part 1 refactoring

--- 2023/day_12/Advent12.kt
+++ 2023/day_12/Advent12.kt
@@ -8,37 +8,41 @@ fun <T : Any> T.debug(a: (T) -> Any = { this }): T = also { if (debug) println(a
 
 fun main(vararg args: String) {
     debug = "-d" in args
-    val records: List<Pair<Record, Signature>> = generateSequence { readlnOrNull()?.trimEnd() }.toList()
+    val records: List<Record> = generateSequence { readlnOrNull()?.trimEnd() }.toList()
         .map { it.split(" ") }
-        .map { it[0] to it[1].split(",").map { n -> n.toInt() } }
+        .map { Record(it[0], it[1].split(",").map { n -> n.toInt() }) }
 
-    records.asSequence().sumOf { (rec, sig) ->
-        arrangements(rec, sig)
-            .debug { "${rec} :: $sig - $it arrangements" }
+    records.asSequence().sumOf {
+        it.arrangements().debug { a -> "$it - $a arrangements" }
     }.also { println("Part 1: $it") }
 }
 
-typealias Record = String
+typealias Spring = String
 typealias Signature = List<Int>
 data class Group(val c: Char, val n: Int)
 
-fun arrangements(record: String, sign: Signature): Long = when {
-    sign.isEmpty() -> if ('#' in record) 0L else 1L
-    record.isEmpty() -> 0L
-    else -> 0L
-        .plusIf(record.canStartWithDot()) { arrangements(record.drop(1), sign) }
-        .plusIf(record.canStartWithHashes(sign.first())) { arrangements(record.drop(sign.first() + 1), sign.drop(1)) }
+data class Record(val spring: Spring, val sign: Signature) {
+    fun arrangements(): Long = when {
+        sign.isEmpty() -> if ('#' in spring) 0L else 1L
+        spring.isEmpty() -> 0L
+        else -> 0L
+            .plusIf(spring.canStartWithDot()) { consumeOneDot().arrangements() }
+            .plusIf(spring.canStartWithHashes(sign.first())) { consumeHashes().arrangements() }
+    }
+
+    fun consumeOneDot() = Record(spring.drop(1), sign)
+    fun consumeHashes() = Record(spring.drop(sign.first() + 1), sign.drop(1))
 }
 
 fun Long.plusIf(condition: Boolean, increment: () -> Long): Long = plus(if (condition) increment() else 0L)
 
-fun String.canStartWithDot() = first() in ".?"
+fun Spring.canStartWithDot() = first() in ".?"
 
-fun String.canStartWithHashes(count: Int): Boolean = take(count).none { it == '.' }
+fun Spring.canStartWithHashes(count: Int): Boolean = take(count).none { it == '.' }
     && drop(count - 1).isNotEmpty()
     && drop(count).firstOrNull()?.let { it != '#' } ?: true
 
-fun String.sign(): Signature = "#+".toRegex().findAll(this)
+fun Spring.sign(): Signature = "#+".toRegex().findAll(this)
     .map { it.value.length }
     .toList()
     .debug { "sign($this) = $it" }
