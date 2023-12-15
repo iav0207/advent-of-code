f34df479611b2eb5d798f277953b24b1c708b59b
Alexander Isaev <IsaevAV27@gmail.com>
2023-12-15 06:42:37 +0100

    2023 Day 15 Part 2

--- 2023/day_15/Advent15.kt
+++ 2023/day_15/Advent15.kt
@@ -10,7 +10,36 @@ fun main(vararg args: String) {
     debug = "-d" in args
     val steps = generateSequence { readlnOrNull()?.trimEnd() }.first().split(",").debug()
     steps.sumOf { hash(it) }.also { println("Part 1: $it") }
+
+    val boxes = MutableList(256) { Box(it) }
+    for (s in steps) when {
+        '-' in s -> s.substring(0, s.length - 1).let { label -> boxes[hash(label)].remove(label) }
+        else -> s.split("=").first().let { label -> boxes[hash(label)].add(s.asLens()) }
+    }
+    println("Part 2: ${boxes.sumOf { it.focusingPower() }}")
 }
 
 fun hash(s: String): Int = s.asSequence().fold(0) { h, it -> h.plus(it.code).times(17).mod(256) }
+
+data class Lens(val label: String, val focalLength: Int)
+fun String.asLens() = split("=").let { Lens(it[0], it[1].toInt()) }
+
+class Box(val number: Int) {
+    private val lenses = mutableListOf<Lens>()
+
+    fun focusingPower(): Long = lenses.mapIndexed { i, le -> 1L.plus(number).times(1L + i).times(le.focalLength) }.sum()
+
+    fun add(lens: Lens) {
+        lenses.withIndex()
+            .find { (_, it) -> it.label == lens.label }
+            ?.also { (i, _) -> lenses[i] = lens }
+            ?: run { lenses.add(lens) }
+        debug { "Box $number: $lenses" }
+    }
+
+    fun remove(label: String) {
+        lenses.removeIf { it.label == label }
+        debug { "Box $number: $lenses" }
+    }
+}
 
