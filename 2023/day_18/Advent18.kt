3977b778d9309529d8916a3065940717e3641008
Alexander Isaev <IsaevAV27@gmail.com>
2023-12-20 21:16:02 +0100

    2023 Day 18 bad

--- 2023/day_18/Advent18.kt
+++ 2023/day_18/Advent18.kt
@@ -29,7 +29,7 @@ fun main(vararg args: String) {
     // count the distance to the closest trench polygon on the odd side,
     // go to the very end
     // divide the count by four
-    // (every inner polygon gets counter from every direction as you traverse the trench)
+    // (every inner polygon gets counted from every direction as you traverse the trench)
     val xIdx = dugOut.groupBy { it.x }
     val yIdx = dugOut.groupBy { it.y }
     cursor = Coord(0, 0)
@@ -39,17 +39,41 @@ fun main(vararg args: String) {
             debug { "cursor=$cursor" }
             when(direc) {
                 "R", "L" -> {
-                    val (down, up) = xIdx[cursor.x]!!.filter { it != cursor }.partition { it.y < cursor.y }
-                    check(down.size.isOdd() != up.size.isOdd()) { "do=$down up=$up" }
+                    val (down, up) = xIdx[cursor.x]!!
+                        .filter { it != cursor }
+                        .partition { it.y < cursor.y }
+
+                    check(down.orthoCount { y }.isOdd() != up.orthoCount { y }.isOdd()) { "do=$down up=$up" }
                 }
                 "U", "D" -> {
-                    val (left, right) = yIdx[cursor.y]!!.filter { it != cursor }.partition { it.x < cursor.x }
-                    check(left.size.isOdd() != right.size.isOdd()) { "le=$left ri=$right" }
+                    val (left, right) = yIdx[cursor.y]!!
+                        .filter { it != cursor }
+                        .partition { it.x < cursor.x }
+
+                    check(left.orthoCount { x }.isOdd() != right.orthoCount { x }.isOdd()) { "le=$left ri=$right" }
                 }
             }
         }
     }
     println("Part 1: ${dugOut.size}")
+}
+
+fun List<Coord>.orthoCount(selector: Coord.() -> Int) = map(selector).orthoCount()
+fun List<Int>.orthoCount(): Int {
+    val sorted = sorted()
+    var prev = sorted.firstOrNull() ?: return 0
+    var result = 1
+    var carry = false
+    for (c in sorted.drop(1)) {
+        if (abs(c - prev) == 1) carry = true
+        else {
+            result += 1
+            carry = false
+        }
+        prev = c
+    }
+    if (carry) result++
+    return result.debug { "${this}.orthoCount() = $it" }
 }
 
 val direcs = mapOf(
@@ -67,7 +91,10 @@ val ortho = mapOf(
 )
 
 data class Coord(val x: Int, val y: Int)
+typealias Vec = Coord
 operator fun Coord.plus(o: Coord) = Coord(x + o.x, y + o.y)
+operator fun Vec.minus(o: Vec) = Vec(x - o.x, y - o.y)
+fun Vec.manhattan() = x + y
 
 fun Int.isOdd() = mod(2) == 0
 
