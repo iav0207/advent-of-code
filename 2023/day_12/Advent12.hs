c7c9a429bf7369abeaaf86ee629f8d46ee0dede9
Alexander Isaev <IsaevAV27@gmail.com>
2024-01-21 15:21:58 +0100

    2023 Day 12 in Haskell: Part 2 (memoization doesn't work)

--- 2023/day_12/Advent12.hs
+++ 2023/day_12/Advent12.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE BangPatterns #-}
-module Advent12 where
+module Main where
 
 import Debug.Trace
+import Data.List
+import Data.Function.Memoize (memoize)
 
-debugMode = True
+debugMode = False
 debug x = if debugMode then trace (show x) x else x
 
 main = interact $ formatOutput . solve
@@ -21,23 +23,29 @@ parseInput = map toRecord . lines where
   replace orig subst = map $ \c -> if c == orig then subst else c
 
 solvePartOne !recs = sum $ map arrangements recs
+solvePartTwo !recs = sum $ map arrangements extended where
+  extended = map extend recs
+  extend (layout, signature) = ((intercalate "?" . replicate 5) layout, (flatten . replicate 5) signature)
+  flatten = foldr (<>) []
 
 arrangements = debug . onDot
 
+-- FIXME: this memoization doesn't work, the program hangs
+
 onDot :: Record -> Integer
-onDot (layout,  []) = if '#' `elem` layout then 0 else 1
-onDot ([], signature) = 0
-onDot (layout@(first:rest), signature) = ifDot + ifHash where
-  ifDot = if first `elem` ".?" then onDot (rest, signature) else 0
-  ifHash = if first `elem` "#?" then onHash (layout, signature) else 0
+onDot = memoize od where
+  od (layout,  []) = if '#' `elem` layout then 0 else 1
+  od ([], signature) = 0
+  od (layout@(first:rest), signature) = ifDot + ifHash where
+      ifDot = if first `elem` ".?" then onDot (rest, signature) else 0
+      ifHash = if first `elem` "#?" then onHash (layout, signature) else 0
 
 onHash :: Record -> Integer
-onHash (layout, []) = 0
-onHash (layout, group:restSign)
-  | '.' `elem` take group layout = 0
-  | length layout < group = 0
-  | (take 1 . drop group) layout == "#" = 0
-  | otherwise = onDot (drop (group + 1) layout, restSign)
-
-solvePartTwo !input = "Not solved"
+onHash = memoize oh where
+  oh (layout, []) = 0
+  oh (layout, group:restSign)
+      | '.' `elem` take group layout = 0
+      | length layout < group = 0
+      | (take 1 . drop group) layout == "#" = 0
+      | otherwise = onDot (drop (group + 1) layout, restSign)
 
