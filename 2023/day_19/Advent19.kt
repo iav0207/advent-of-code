89dd73b8e57669c2fe7c54ae46158511d39e5a14
Alexander Isaev <IsaevAV27@gmail.com>
2024-03-03 16:44:41 +0100

    Refactoring @ 2023 Day 19

--- 2023/day_19/Advent19.kt
+++ 2023/day_19/Advent19.kt
@@ -15,36 +15,85 @@ fun main(vararg args: String) {
         .run { first.map { it.asItem() } to second.map { it.asWorkflow() }.associateBy { it.name } }
 
     items.filter { it.isAcceptedBy(workflows) }
-        .sumOf { it.x + it.m + it.a + it.s }
+        .sumOf { it.values.sum() }
         .also { println("Part 1: $it") }
 
     println("Part 2: ${countDistinctAcceptedItems(workflows)}")
 }
 
+fun String.asItem() = dimensions.toList().zip(parseInts()).toMap()
+fun String.parseInts() = intsRegex.findAll(this).map { it.value.toInt() }.toList()
+val intsRegex = "\\d+".toRegex()
+
+fun String.asWorkflow() = split("[{}]".toRegex()).run { Workflow(get(0), get(1).split(",")) }
+
+typealias Item = Map<Char, Val>
+typealias Val = Int
+
+typealias Workflows = Map<String, Workflow>
+class Workflow(val name: String, val branches: List<Branch>)
+typealias Branch = String
+val Branch.goto get() = substringAfter(":")
+
 fun Item.isAcceptedBy(wfs: Workflows): Boolean {
     var wfId = "in"
     while (wfId !in listOf("A", "R")) {
         val wf = wfs[wfId] ?: error("unknown workflow: $wfId")
-        wfId = wf.branches.first { it.condition(this) }.goto
+        wfId = wf.branches.first { branch ->
+            when {
+                ":" !in branch -> true
+                else -> {
+                    val prop = get(branch[0])!!
+                    val v = branch.parseInts()[0]
+                    val cmp = prop.compareTo(v)
+                    if (">" in branch) cmp > 0 else cmp < 0
+                }
+            }
+        }.goto
     }
     return wfId == "A"
 }
 
-val dimensions = "xmas"
+fun countDistinctAcceptedItems(wfs: Workflows): Long {
+    var count = 0L
+    val todo = ArrayDeque<State>().apply { add(State("in", HyperRange())) }
 
+    while (todo.isNotEmpty()) {
+        val (wfName, cube) = todo.removeFirst()
+        if (wfName == "A") count += cube.volume
+        if (wfName in "AR") continue
+        val wf = wfs[wfName]!!
+
+        wf.branches.fold(cube) { accCube, branch ->
+            if (":" in branch) {
+                val (ifYes, ifNot) = accCube.partition(branch)
+                todo.addLast(State(branch.goto, ifYes))
+                ifNot
+            } else {
+                todo.addLast(State(branch, accCube))
+                accCube
+            }
+        }
+    }
+
+    return count
+}
+
+val dimensions = "xmas"
 
 data class HyperRange(
     val axes: Map<Char, Range1> = dimensions.associateWith { Range1() }
 ) {
     val volume get() = axes.values.map { it.length }.product()
-    fun partition(cmd: String): Pair<HyperRange, HyperRange> {
-        val dim = cmd[0]
-        val pivot = cmd.parseInts()[0]
+
+    fun partition(branch: String): Pair<HyperRange, HyperRange> {
+        val dim = branch[0]
+        val pivot = branch.parseInts()[0]
         return axes[dim]!!.let {
-            when(cmd[1]) {
+            when(branch[1]) {
                 '>' -> { it.partitionGreaterThan(pivot) }
                 '<' -> { it.partitionLessThan(pivot) }
-                else -> error("unknown operation ${cmd[1]} in $cmd")
+                else -> error("unknown operation ${branch[1]} in $branch")
             }
         }.let { ranges -> ranges.map { it -> modify(dim, it) } }
     }
@@ -67,70 +116,5 @@ data class Range1(val min: Val = 1, val max: Val = 4000) {
     val length = maxOf(0, max - min + 1).toLong()
 }
 
-fun countDistinctAcceptedItems(wfs: Workflows): Long {
-    var count = 0L
-    val todo = ArrayDeque<State>().apply { add(State("in", HyperRange())) }
-
-    while (todo.isNotEmpty()) {
-        val (wfName, cube) = todo.removeFirst()
-        if (wfName == "A") count += cube.volume
-        if (wfName in "AR") continue
-        val wf = wfs[wfName]!!
-
-        wf.branches.map { it.src }.fold(cube) { accCube, cmd ->
-            if (":" in cmd) {
-                val (ifYes, ifNot) = accCube.partition(cmd)
-                todo.addLast(State(cmd.substringAfter(":"), ifYes))
-                ifNot
-            } else {
-                todo.addLast(State(cmd, accCube))
-                accCube
-            }
-        }
-    }
-
-    return count
-}
-
 data class State(val wfName: String, val cube: HyperRange)
-
-fun String.asItem() = parseInts().run { Item(get(0), get(1), get(2), get(3)) }
-fun String.parseInts() = intsRegex.findAll(this).map { it.value.toInt() }.toList()
-val intsRegex = "\\d+".toRegex()
-
-fun String.asWorkflow() = split("[{}]".toRegex()).run {
-    Workflow(get(0), get(1).split(",").map { it.asBranch() })
-}
-fun String.asBranch() = when {
-    ":" !in this -> Branch({ true }, this, this)
-    else -> {
-        check("<" in this || ">" in this) { "cannot parse branch: $this" }
-        val constant = parseInts()[0]
-        val getProp = first().asGetter()
-        Branch(
-            condition = {
-                val cmp = it.getProp().compareTo(constant)
-                if ("<" in this) cmp < 0 else cmp > 0
-            },
-            goto = substringAfter(":"),
-            src = this,
-        )
-    }
-}
-fun Char.asGetter(): Item.() -> Val = when(this) {
-    'x' -> then { x }
-    'm' -> then { m }
-    'a' -> then { a }
-    's' -> then { s }
-    else -> error("unknown prop: " + this)
-}
-fun <T> then(x: T): T = x
-
-data class Item(val x: Val, val m: Val, val a: Val, val s: Val)
-typealias Val = Int
-
-typealias Workflows = Map<String, Workflow>
-class Workflow(val name: String, val branches: List<Branch>)
-class Branch(val condition: Condition, val goto: String, val src: String)
-typealias Condition = (Item) -> Boolean
 
